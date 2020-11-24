74c6f63d942ec863d7edcfc4e0ab2f1b95953836
IvanS <ivan443931@gmail.com>
2020-11-25 02:13:41 +0300

    Add Vuetify

--- src/Niagara.Web/ClientApp/src/main.js
+++ src/Niagara.Web/ClientApp/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ Vue.use(VueAxios, axios)
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/Niagara.Web/ClientApp/src/views/MaterialLogsList.vue
+++ src/Niagara.Web/ClientApp/src/views/MaterialLogsList.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="material-logs-list">
     Material Logs
-    <table>
-      <tr>
-        <th>
-          Lot #
-        </th>
-        <th>
-          Description
-        </th>
-        <th>
-          Date Created
-        </th>
-        <th>
-          Date Updated
-        </th>
-      </tr>
-      <tr v-for="materialLog in materialLogs" :key="materialLog.lotNumber">
-        <td>
-          {{ materialLog.lotNumber }}
-        </td>
-        <td>
-          {{ materialLog.description }}
-        </td>
-        <td>
-          {{ materialLog.dateCreated }}
-        </td>
-        <td>
-          {{ materialLog.dateUpdated }}
-        </td>
-      </tr>
-    </table>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Lot #
+            </th>
+            <th class="text-left">
+              Description
+            </th>
+            <th class="text-left">
+              Date Created
+            </th>
+            <th class="text-left">
+              Date Updated
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="materialLog in materialLogs" :key="materialLog.lotNumber">
+            <td>
+              {{ materialLog.lotNumber }}
+            </td>
+            <td>
+              {{ materialLog.description }}
+            </td>
+            <td>
+              {{ materialLog.dateCreated }}
+            </td>
+            <td>
+              {{ materialLog.dateUpdated }}
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
 </template>
 
